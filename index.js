fa0bf79e9640d83bbc30494b30133b9ae09fd0f7
mrsshadoww <subsanik@gmail.com>
2018-07-19 23:53:48 +0300

    fix

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ const autobazar = require('./moduls/autobazar.js')
 
 /* Your code*/
 
-let ab = new autobazar();
-
-ab.getPosts('https://ab.ua/avto/?capacityUnit=1&currency=uah&isNew=0&powerUnit=1&transport=1');
+autobazar.getPosts('https://ab.ua/avto/?capacityUnit=1&currency=uah&isNew=0&powerUnit=1&transport=1');
--- moduls/autobazar.js
+++ moduls/autobazar.js
@@ -1,30 +1,30 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-let autobazar = function () {
-	let getPost = function(url) {
+let autobazar = {
+	getPost: function(url) {
 		request(url, function (err, res, body) {
-          if (err) throw err;
+               if (err) throw err;
 
-          let $ = cheerio.load(res.body);
-          let post = {};
+               let $ = cheerio.load(res.body);
+               let post = {};
 
-          post.name    = $('div>h2').text();
-          post.prise   = $('div.QSUFp>div:nth-child(1)').text();
-          post.year    = $('.RyC3G:nth-child(1) div._29-AP').text();
-          post.mileage = $('.RyC3G:nth-child(2) div._29-AP').text();
-          post.kyzov   = $('.RyC3G:nth-child(4) div._29-AP').text();
-          post.type    = $('.RyC3G:nth-child(5) div._29-AP').text();
-          post.color   = $('.RyC3G:nth-child(6) div._29-AP').text();
-          post.engine  = $('.RyC3G:nth-child(7) div._29-AP').text();
-          post.box     = $('.RyC3G:nth-child(8) div._29-AP').text();
-          post.privod  = $('.RyC3G:nth-child(9) div._29-AP').text();
-          post.customs = $('.RyC3G:nth-child(10) div._29-AP').text();
+               post.name    = $('div>h2').text();
+               post.prise   = $('div.QSUFp>div:nth-child(1)').text();
+               post.year    = $('.RyC3G:nth-child(1) div._29-AP').text();
+               post.mileage = $('.RyC3G:nth-child(2) div._29-AP').text();
+               post.kyzov   = $('.RyC3G:nth-child(4) div._29-AP').text();
+               post.type    = $('.RyC3G:nth-child(5) div._29-AP').text();
+               post.color   = $('.RyC3G:nth-child(6) div._29-AP').text();
+               post.engine  = $('.RyC3G:nth-child(7) div._29-AP').text();
+               post.box     = $('.RyC3G:nth-child(8) div._29-AP').text();
+               post.privod  = $('.RyC3G:nth-child(9) div._29-AP').text();
+               post.customs = $('.RyC3G:nth-child(10) div._29-AP').text();
 
-          console.log(post);
-     });
-	}
-	this.getPosts = function(URL) {
+               console.log(post);
+          });
+	},
+	getPosts: function(URL) {
 	    request(URL, function (err, res, body) {
 	          if (err) throw err;
 
@@ -33,7 +33,7 @@ let autobazar = function () {
 	          $('div._2KGO4 ._1i-so a.b8dUr').each(function() {
 	               let url = 'https://ab.ua' + $(this).attr('href')
 
-	               getPost(url)
+	               autobazar.getPost(url)
 
 	          });
 	     });
